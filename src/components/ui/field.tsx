01076c2a9fc019e83797406d35e58a9f4fd6e6dc
k-jdev <svv.ooj@gmail.com>
2026-03-02 21:47:29 +0200

    update

--- src/components/ui/field.tsx
+++ src/components/ui/field.tsx
@@ -104,7 +104,7 @@ function FieldLabel({
     <Label
       data-slot="field-label"
       className={cn(
-        "group/field-label peer/field-label flex w-fit gap-2 leading-snug group-data-[disabled=true]/field:opacity-50 has-data-checked:border-primary/30 has-data-checked:bg-primary/5 has-[>[data-slot=field]]:rounded-xl has-[>[data-slot=field]]:border has-[>[data-slot=field]]:transition-colors has-[>[data-slot=field]]:not-has-data-checked:hover:bg-input/10 has-[>[data-slot=field]]:not-has-data-checked:hover:text-foreground *:data-[slot=field]:p-4 dark:has-data-checked:border-primary/20 dark:has-data-checked:bg-primary/10",
+        "group/field-label peer/field-label flex w-fit gap-2 leading-snug group-data-[disabled=true]/field:opacity-50 has-data-checked:border-primary/30 has-data-checked:bg-primary/5 has-[>[data-slot=field]]:rounded-xl has-[>[data-slot=field]]:border has-[>[data-slot=field]]:transition-colors has-[>[data-slot=field]]:not-has-data-checked:hover:bg-input/20 has-[>[data-slot=field]]:not-has-data-checked:hover:text-foreground dark:has-[>[data-slot=field]]:not-has-data-checked:hover:bg-input/20 *:data-[slot=field]:p-4 dark:has-data-checked:border-primary/20 dark:has-data-checked:bg-primary/10",
         "has-[>[data-slot=field]]:w-full has-[>[data-slot=field]]:flex-col",
         className,
       )}
